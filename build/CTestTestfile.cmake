a5ebd094715149713cada0c64e8c1d1b7719a168
Ricardo Cordeiro <rcordeiro@cardpresso.com>
2023-05-04 14:32:22 +0100

    helloworld-win

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/rcordeiro/helloworld_win
+# Build directory: C:/Users/rcordeiro/helloworld_win/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
